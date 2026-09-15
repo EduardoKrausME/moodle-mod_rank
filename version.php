f54355fb8c8bfa3942d26515cdb2d67a338bbc2c
Eduardo Kraus <kraus@eduardokraus.com>
2026-09-15 12:15:43 -0300

    V 1.1.1

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2026091400;
-$plugin->release = "1.1.0";
+$plugin->version = 2026091500;
+$plugin->release = "1.1.1";
 $plugin->component = "mod_rank";
 $plugin->requires = 2022041900;
 $plugin->maturity = MATURITY_STABLE;
